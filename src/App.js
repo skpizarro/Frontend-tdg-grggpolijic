409cb64b0d7a41ddce142ff984ab80af8e1770cd
Santiago <santi_diaz17@hotmail.com>
2019-06-20 09:12:47 -0500

    to-dos

--- src/App.js
+++ src/App.js
@@ -18,8 +18,34 @@ export default class App extends Component {
     })
 
     console.log(usuarios);
-
  }
+
+
+ /**
+  * ---------------------Front End---------------------------------------
+  * 
+  * Diseño del header -- Cambiar a centrado con todo el fondo amarillo 
+  * 
+  * Modificar vista nosotros en la seccion titulo poner info de la granja 
+  * Modificar captura de campos quitar Ref ---> poner Handler inputs //
+  *  Revisar Validaciones de campos: nombre - apellido solo texto
+  * id - celular solo numerico entradas numericas(que no permita ingresar numeros)
+  * entradas de fecha validar que no se pueda escoger una fecha anterior al dia actual
+  * Crear modal con terminos y condiciones 
+  * Envio post (Axios) de los datos del usuario 
+  * recibir un respuesta y mostrar mensaje de qr enviado exitosamente al correo
+  * 
+  * (EndPoints en el drive)-----
+  * 
+  * axios.post(endpoint, (
+  * 
+  * usuario // objeto como tal 
+  * ))
+  * 
+  * --------------------Raspberry ------------------------------------------------
+  * descargar librerias y probar que funcione
+  * 
+  */
 
   render() {
     return (
